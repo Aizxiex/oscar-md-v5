034c81c4565b4e3adf7870a5cebf282be0e76813
Aizxiex <117944640+Aizxiex@users.noreply.github.com>
2022-11-11 08:55:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-global.you = ['6285850539404'] 
+global.you = ['6281243364255'] 
 //Developer Bot
 global.gc = 'https://chat.whatsapp.com/I5Kz3Abawc5DqyBSDoYvKB'
-global.rowner = ['6285850539404'] // Real Owner Bot
-global.owner = ['6285850539404'] // Owner Bot
-global.police = ['6285850539404'] // Police Bot
-global.mods = ['6285850539404']  // Want some help?
-global.prems = ['6285850539404'] // Premium user has unlimited limit
+global.rowner = ['6281243364255'] // Real Owner Bot
+global.owner = ['6281243364255'] // Owner Bot
+global.police = ['6281243364255'] // Police Bot
+global.mods = ['6281243364255']  // Want some help?
+global.prems = ['6281243364255'] // Premium user has unlimited limit
 global.fotonya1 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini kalo mau di ganti silahkan ganti aja
 global.fotonya2 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //gw lupa nih foto apa aja:v
 global.fotonya3 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
@@ -15,7 +15,7 @@ global.fotonya6 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini gant
 global.fotonya7 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
 global.instagram = 'https://instagram.com/Jarotr_'
 
-global.helper = ['6285850539404']
+global.helper = ['6281243364255']
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
